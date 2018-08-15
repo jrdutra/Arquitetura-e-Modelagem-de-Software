041c50365d882dcd996815776f999bad46a09d92
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-15 13:07:02 -0300

    primeiro-commit

---
+++ primeiro-projeto/engsoftware/src/main/java/projeto1/engsoftware/App.java
@@ -0,0 +1,13 @@
+package projeto1.engsoftware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
