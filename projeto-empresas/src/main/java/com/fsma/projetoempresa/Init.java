9d7efb70dcfaa879f98d179dc6954db2889a7ebf
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-16 18:51:18 -0300

    gf

--- projeto-empresas/src/main/java/com/fsma/projetoempresa/Init.java
+++ projeto-empresas/src/main/java/com/fsma/projetoempresa/Init.java
@@ -3,7 +3,6 @@ package com.fsma.projetoempresa;
 public class Init {
 	public static void main(String[] args) {
 		Leitor l = new Leitor("C:\\Users\\jrdut\\Desktop\\JAVA\\Arquitetura-e-Modelagem-de-Software\\projeto-empresas\\teste.csv", ";");
-		System.out.println(l.currentApplicationDir());
 		l.readCsvFile();
 	}
 }
