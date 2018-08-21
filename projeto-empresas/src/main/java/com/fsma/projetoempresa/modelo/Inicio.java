340245058f24db7a90b2beac3b0f78e052e4b7cc
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-21 14:15:32 -0300

    fds

--- projeto-empresas/src/main/java/com/fsma/projetoempresa/modelo/Inicio.java
+++ projeto-empresas/src/main/java/com/fsma/projetoempresa/modelo/Inicio.java
@@ -1,10 +1,9 @@
 package com.fsma.projetoempresa.modelo;
 
 import com.fsma.projetoempresa.conexao.JPAUtil;
-import com.fsma.projetoempresa.dao.EmpresaDao;
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
-
+//import com.fsma.projetoempresa.dao.EmpresaDao;
 //import javax.persistence.EntityManager;
 //import com.fsma.projetoempresa.conexao.JPAUtil;
 //import com.fsma.projetoempresa.dao.EmpresaDao;
@@ -16,17 +15,11 @@ public class Inicio {
 	public static void main(String[] args) {
 		ArrayList<Empresa> lista_de_empresas = new ArrayList<Empresa>();
 		ArrayList<Empresa> lista_de_empresas_tratadas = new ArrayList<Empresa>();
-		Leitor leitor = new Leitor("C:\\Users\\jrdut\\Desktop\\JAVA\\Arquitetura-e-Modelagem-de-Software\\projeto-empresas\\teste.csv", ";");
+		Leitor leitor = new Leitor("C:\\Users\\jrdut\\Desktop\\JAVA\\Arquitetura-e-Modelagem-de-Software\\projeto-empresas\\fiscalizacao_Sao_Paulo.csv", ";");
 		lista_de_empresas = leitor.getTodasEmpresas();
 		lista_de_empresas_tratadas = leitor.trataTodasEmpresas(lista_de_empresas);
 		Empresa empresa = new Empresa();	
-		
-		
-		
-		
-		
 		EntityManager em = JPAUtil.getEntityManager();
-		EmpresaDao empresaDao = new EmpresaDao(em);
 		
 		
 		
@@ -36,7 +29,7 @@ public class Inicio {
 		em.persist(empresa);
 		em.getTransaction().commit();
 		*/
-		
+		System.out.println("Gravando no banco..");
 		try {
 			
 			em.getTransaction().begin();
@@ -51,7 +44,7 @@ public class Inicio {
 			em.getTransaction().rollback();
 		}
 		
+		System.out.println("Fim...");
 		
-		System.out.println(empresa);
 	}
 }
