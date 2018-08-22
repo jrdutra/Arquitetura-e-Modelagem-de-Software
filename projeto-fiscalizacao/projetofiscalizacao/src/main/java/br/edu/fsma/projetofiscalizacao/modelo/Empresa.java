99de6719ca1114cef7ace7148562548787cf973a
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-22 15:50:27 -0300

    56

--- projeto-fiscalizacao/projetofiscalizacao/src/main/java/br/edu/fsma/projetofiscalizacao/modelo/Empresa.java
+++ projeto-fiscalizacao/projetofiscalizacao/src/main/java/br/edu/fsma/projetofiscalizacao/modelo/Empresa.java
@@ -1,6 +1,9 @@
 package br.edu.fsma.projetofiscalizacao.modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,65 +25,56 @@ public class Empresa implements Serializable{
 	
 	private String cnpj;
 	private String razaosocial;
+	private ArrayList<Fiscalizacao> fiscalizacoes = new ArrayList<Fiscalizacao>();
 	
 	private String logradouro;
 	private String cep;
 	private Bairro bairro;
 	
+	public ArrayList<Fiscalizacao> getFiscalizacoes() {
+		return (ArrayList<Fiscalizacao>) Collections.unmodifiableList(fiscalizacoes);
+	}
+	
+	public void setFiscalizacoes(ArrayList<Fiscalizacao> fiscalizacoes) {
+		this.fiscalizacoes = fiscalizacoes;
+	}
+	
 	public Long getId() {
 		return id;
 	}
-
-
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getCnpj() {
 		return cnpj;
 	}
 
-
-
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-
-
 
 	public String getRazaosocial() {
 		return razaosocial;
 	}
 
-
-
 	public void setRazaosocial(String razaosocial) {
 		this.razaosocial = razaosocial;
 	}
-
-
 
 	public Bairro getBairro() {
 		return bairro;
 	}
 
-
-
 	public void setBairro(Bairro bairro) {
 		this.bairro = bairro;
 	}
-
-
+	
 	@Override
 	public String toString() {
 		return "\n" + cnpj + " " + razaosocial;
 	}
-
-
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -88,14 +82,12 @@ public class Empresa implements Serializable{
 		result = prime * result + ((bairro == null) ? 0 : bairro.hashCode());
 		result = prime * result + ((cep == null) ? 0 : cep.hashCode());
 		result = prime * result + ((cnpj == null) ? 0 : cnpj.hashCode());
+		result = prime * result + ((fiscalizacoes == null) ? 0 : fiscalizacoes.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((logradouro == null) ? 0 : logradouro.hashCode());
 		result = prime * result + ((razaosocial == null) ? 0 : razaosocial.hashCode());
 		return result;
 	}
-
-
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -120,6 +112,11 @@ public class Empresa implements Serializable{
 				return false;
 		} else if (!cnpj.equals(other.cnpj))
 			return false;
+		if (fiscalizacoes == null) {
+			if (other.fiscalizacoes != null)
+				return false;
+		} else if (!fiscalizacoes.equals(other.fiscalizacoes))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -137,7 +134,6 @@ public class Empresa implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 	
 }
