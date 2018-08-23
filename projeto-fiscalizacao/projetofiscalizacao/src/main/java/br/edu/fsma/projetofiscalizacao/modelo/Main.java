3603babde8af69b589c81f6090876214bf5f1eb0
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-08-23 16:32:09 -0300

    re

--- projeto-fiscalizacao/projetofiscalizacao/src/main/java/br/edu/fsma/projetofiscalizacao/modelo/Main.java
+++ projeto-fiscalizacao/projetofiscalizacao/src/main/java/br/edu/fsma/projetofiscalizacao/modelo/Main.java
@@ -1,9 +1,33 @@
 package br.edu.fsma.projetofiscalizacao.modelo;
 
+import javax.persistence.EntityManager;
+
+import br.edu.fsma.projetofiscalizacao.conexao.JPAUtil;
+import br.edu.fsma.projetofiscalizacao.dao.EmpresaDAO;
+import br.edu.fsma.projetofiscalizacao.dao.FiscalizacaoDAO;
+import br.edu.fsma.projetofiscalizacao.dao.BairroDAO;
+import br.edu.fsma.projetofiscalizacao.dao.MunicipioDAO;
+import br.edu.fsma.projetofiscalizacao.dao.UfDAO;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("TESTE");
+		
+		//EXEMPLO DE FISCALIZACAO DO ARQUIVO:
+		//2010;2010/06;96.534.094/0001-58;ACCENTURE DO BRASIL LTDA;RUA ALEXANDRE DUMAS,2051;04717-004;CHAC SANTO ANTONIO;São Paulo;São Paulo
+		
+		Empresa empresa = new Empresa();
+		Fiscalizacao fiscalizacao = new Fiscalizacao();
+		Bairro bairro = new Bairro();
+		Municipio municipio = new Municipio();
+		Uf uf = new Uf();
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		EmpresaDAO empresadao = new EmpresaDAO(em);
+		FiscalizacaoDAO fiscalizacaodao = new FiscalizacaoDAO(em);
+		BairroDAO bairrodao = new BairroDAO(em);
+		MunicipioDAO municipiodao = new MunicipioDAO(em);
+		UfDAO ufdao = new UfDAO(em);
 		
 	}
 }
