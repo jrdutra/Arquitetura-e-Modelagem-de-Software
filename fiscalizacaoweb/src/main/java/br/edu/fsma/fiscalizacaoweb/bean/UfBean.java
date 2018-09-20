35b55b2a4de9af4ebcaec1b08c0c56b181ea72d3
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-20 17:02:56 -0300

    atualizando e capturando inputtext.

--- fiscalizacaoweb/src/main/java/br/edu/fsma/fiscalizacaoweb/bean/UfBean.java
+++ fiscalizacaoweb/src/main/java/br/edu/fsma/fiscalizacaoweb/bean/UfBean.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.fsma.fiscalizacaoweb.modelo.negocio.Uf;
+
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Named
@@ -16,5 +19,22 @@ public class UfBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private List<Uf> ufs = new ArrayList<Uf>();
 	private Uf currentUf = new Uf();
+	
+	public void capturaDadosPesquisa() {
+		  HttpServletRequest request = (HttpServletRequest) FacesContext
+						.getCurrentInstance().getExternalContext().getRequest();
+		  currentUf.setNome(request.getParameter("frm_pesquisar_uf:input_nome_pesquisa"));
+		  currentUf.setSigla(request.getParameter("frm_pesquisar_uf:input_sigla_pesquisa"));
+		  System.out.println("AQUI!");
+	}
+	
+	public Uf getCurrentUf() {
+		return currentUf;
+	}
+	public void setCurrentUf(Uf currentUf) {
+		this.currentUf = currentUf;
+	}
+	
+	
 	
 }
