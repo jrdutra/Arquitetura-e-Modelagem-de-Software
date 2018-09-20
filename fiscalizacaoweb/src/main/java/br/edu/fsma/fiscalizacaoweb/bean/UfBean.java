1248bb3c918e575a1fb05828d8ba763f6d34aeef
João Ricardo Côre Dutra <jrdutra_@msn.com>
2018-09-20 17:42:18 -0300

    Incluindo

--- fiscalizacaoweb/src/main/java/br/edu/fsma/fiscalizacaoweb/bean/UfBean.java
+++ fiscalizacaoweb/src/main/java/br/edu/fsma/fiscalizacaoweb/bean/UfBean.java
@@ -19,13 +19,47 @@ public class UfBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private List<Uf> ufs = new ArrayList<Uf>();
 	private Uf currentUf = new Uf();
+	private enum Nome {PAINELINCLUIR, PAINELPESQUISAR};
+	private Nome nome = Nome.PAINELPESQUISAR;
 	
 	public void capturaDadosPesquisa() {
 		  HttpServletRequest request = (HttpServletRequest) FacesContext
 						.getCurrentInstance().getExternalContext().getRequest();
-		  currentUf.setNome(request.getParameter("frm_pesquisar_uf:input_nome_pesquisa"));
-		  currentUf.setSigla(request.getParameter("frm_pesquisar_uf:input_sigla_pesquisa"));
-		  System.out.println("AQUI!");
+		  currentUf.setNome(request.getParameter("frmpesquisar:input_nome_pesquisa"));
+		  currentUf.setSigla(request.getParameter("frmpesquisar:input_sigla_pesquisa"));
+	}
+	
+	public void incluirClick() {
+		
+		capturaDadosPesquisa();
+		setIncluir();
+	}
+	
+	public void okClick() {
+		setPesquisar();
+	}
+	
+	public String getIdToUpdate() {
+		return (":frmpesquisar "
+				+ ":frmResultado "
+				+ ":frmnovauf"
+				);
+	}
+	
+	public void setIncluir() {
+		nome = Nome.PAINELINCLUIR;
+	}
+	
+	public boolean isMostraIncluir() {
+		return (nome == Nome.PAINELINCLUIR);
+	}
+	
+	public void setPesquisar() {
+		nome = Nome.PAINELPESQUISAR;
+	}
+	
+	public boolean isMostraPesquisar() {
+		return (nome == Nome.PAINELPESQUISAR);
 	}
 	
 	public Uf getCurrentUf() {
